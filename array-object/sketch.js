f669e92e3818a188fd7bd56c78ef22a63e198738
dwliv <dinel.rw123@gmail.com>
2024-10-15 14:15:03 -0600

    cleaning up code and adding obstacles

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,17 +5,53 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const CHOICE = ["Rock", "Paper", "Scissors"];
-const DEFAULT_CHOICE = ["Rock"];
+let bx = 200;  
+let by = 200; 
+let dx = 5; 
+let dy = 3; 
+let ballSize = 10;
+let obstacleArray = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  displayrects();
+  player();
+  ballSize();
 }
 
-function startGame(CHOICE, DEFAULT_CHOICE) {
-  let player_choice = prompt("Please pick Rock or Paper or Scissors .");
+function player() {
+
+}
+
+function spawnObsticles() {
+  let someRect = {
+    x: random(0, width),
+    y: height + random(0, 50),
+    speed: random(2, 5),
+    radius: random(20, 50),
+    r: random(255),
+    g: random(255),
+    b: random(255),
+    alpha: random(255),
+    timeX: random(100000000),
+    timeY: random(100000000),
+    deltaTime: 0.006,
+  };
+  theBubbles.push(someBubble);
+}
+
+function clickedOnBubble(x, y, theBubble) {
+  let distanceAway = dist(x, y, theBubble.x, theBubble.y); 
+  return distanceAway < theBubble.radius;
+}
+function displayrects() {
+  for (let bubble of theBubbles) {
+    noStroke();
+    fill(bubble.r, bubble.g, bubble.b, bubble.alpha);
+    circle(bubble.x, bubble.y, bubble.radius * 2);
+  }
 }
