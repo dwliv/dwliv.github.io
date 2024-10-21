8595a8163a5f41be219b8c7892c7557ed488d461
dwliv <dinel.rw123@gmail.com>
2024-10-21 14:13:27 -0600

    did the final touches and cleaned up my code

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -3,7 +3,7 @@
 // October 21, 2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// I could be mistaken but I don't think we've usen noLoop() in a demo before.
 
 let turtles;
 let finishLine;
@@ -11,47 +11,66 @@ let winner;
 let r = 200;
 let b = 200;
 let g = 255;
+let bgMusic;
+
+function preLoad() {
+  bgMusic = loadSound("tune.wav");
+}
 
 function setup() {
-  createCanvas(800, 500);
-  finishLine = width - 50;
-  resetRace();
+  createCanvas(800, 500); // canvas 800 pixels wide and 500 pixels high
+  finishLine = width - 50; // draws the finish line on the edge 50 pixels from the right
+  resetRace(); // resets the race after the previous one has finished
 }
 
 function draw() {
   background(r, g, b);
-  stroke(0);
+  startGame();
+}
+
+function startGame() {
+  //Draws the finish line
+  stroke(0); //makes the finish line appear black
   line(finishLine, 0, finishLine, height);
+
+  //Draws the turtle with their appropriate color
   for(let turtle of turtles) {
     fill(turtle.color);
     noStroke();
-    ellipse(turtle.x, turtle.y, 50, 50);  
-    turtle.x += random(1, 4);
-    if (turtle.x >= finishLine && !winner) {
+
+    // the shape of the turtles
+    ellipse(turtle.x, turtle.y, 50, 50);
+    
+    // moves the turtle randomly by 1-5 pixels
+    turtle.x += random(1, 5); 
+
+    // checks for a winner
+    if (turtle.x >= finishLine && !winner) { 
       winner = turtle.color;
     }
   }
   if (winner) {
-    textSize(35);
-    fill(0);
-    text(winner + " wins!", 50, height / 2);
-    noLoop(); //stops the draw loop
+    textSize(35); //how big the text is when the winner is called
+    fill(0); // makes the text appear in the color black
+    text(winner + " wins!", 50, height / 2); // displays "turtle.color wins!" with a width of 50 and a height divided by 2
+    noLoop(); //stops the draw loop after a winner is picked so it doesn't go infinitely
   }
 }
 
+
 function resetRace() {
   turtles = [
-    {x: 50, y: 100, color: "red" },
-    {x: 50, y: 200, color: "blue" },
-    {x: 50, y: 300, color: "green" },
-    {x:50, y:400, color: "purple" },
+    {x: 50, y: 100, color: "red" }, //makes the red turtle
+    {x: 50, y: 200, color: "blue" }, //makes the blue turtle
+    {x: 50, y: 300, color: "green" }, //makes the green turtle
+    {x:50, y:400, color: "purple" }, ////makes the purple turtle
   ];
   winner = null; // no value for now
 }
 function keyPressed() {
   if (winner) {
-    resetRace;
-    loop();//restarts the draw loop
+    resetRace();
+    loop();//restarts the draw loop after a key is pressed
   }
 }
 
