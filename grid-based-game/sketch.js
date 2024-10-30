50190959f335c456d901835db0484c27ed04cba3
dwliv <dinel.rw123@gmail.com>
2024-10-30 14:11:27 -0600

    added the grid/arena, the player, and the gems, gonna put a scoreboard next work period

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -1,15 +1,44 @@
-// Project Title
-// Your Name
-// Date
+//Grid-based game
+// Dinel Widyaratne
+// Friday, November 8, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let cols = 10;
+let rows  = 10;
+let cellSize = 25;
+
+let player;
+let gem;
+let score = 0;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(cols * cellSize, rows * cellSize);
+  player = createVector(0, 0);
+  placeGem();
 }
 
 function draw() {
   background(220);
+  stroke(0);
+  for(let i = 0; i < cols; i++) {
+    for(let j = 0; j < rows; j++) {
+      noFill();
+      rect(i * cellSize, j * cellSize, cellSize, cellSize);
+    }
+  }
+  fill(0, 0, 255);
+  rect(player.x * cellSize, player.y*cellSize, cellSize, cellSize);
+
+  fill(255,0,255);
+  rect(gem.x*cellSize, gem.y*cellSize, cellSize, cellSize);
+
+  textSize(15);
+  
+}
+
+function placeGem() {
+  gem = createVector(floor(random(cols)),floor(random(rows)));
 }
